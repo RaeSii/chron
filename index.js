a2a139a2f4d7428fbaaeb2b0fb4d1f3664aea26f
raesii <leisolo@gmail.com>
2022-05-28 09:43:30 -0700

    added user register, login resolvers and validators

--- index.js
+++ index.js
@@ -2,34 +2,21 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const typeDefs = require('./server/typeDefs')
-const resolvers = require('./server/resolvers')
+const Post = require('./models/Post');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers/index');
 const { MONGODB } = require('./config');
 
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            } catch (err) {
-                throw new Error(err);
-            }
-        }
-    }
-};
-
-const server = new ApolloServer ({
-    typeDefs,
-    resolvers
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 });
-
-mongoose.connect(MONGODB, { useNewUrlParser: true})
-.then(() => {
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port:5000})
- 
-})
-.then(res => {
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
     console.log(`Server running at ${res.url}`);
-})
+  });
